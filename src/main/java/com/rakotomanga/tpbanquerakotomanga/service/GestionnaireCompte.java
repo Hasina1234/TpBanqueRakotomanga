a6833da4f1ea13af77ab56d471472c3645457aa9
Hasina1234 <89484785+Hasina1234@users.noreply.github.com>
2024-04-12 22:18:21 +0300

    Écriture d'un bean CDI GestionnaireCompte

---
+++ src/main/java/com/rakotomanga/tpbanquerakotomanga/service/GestionnaireCompte.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.rakotomanga.tpbanquerakotomanga.service;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Hasina
+ */
+@Named(value = "gestionnaireCompte")
+@RequestScoped
+public class GestionnaireCompte {
+
+    /**
+     * Creates a new instance of GestionnaireCompte
+     */
+    public GestionnaireCompte() {
+    }
+    
+}
