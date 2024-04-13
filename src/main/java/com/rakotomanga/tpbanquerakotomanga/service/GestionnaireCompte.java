f26d884fe4c0df4e299d098ed72867b0e96a4dd7
Hasina1234 <89484785+Hasina1234@users.noreply.github.com>
2024-04-13 13:37:59 +0300

    Code du bean CDI

--- src/main/java/com/rakotomanga/tpbanquerakotomanga/service/GestionnaireCompte.java
+++ src/main/java/com/rakotomanga/tpbanquerakotomanga/service/GestionnaireCompte.java
@@ -4,21 +4,56 @@
  */
 package com.rakotomanga.tpbanquerakotomanga.service;
 
+import com.rakotomanga.tpbanquerakotomanga.entities.CompteBancaire;
+import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
 
 /**
  *
  * @author Hasina
  */
+
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",              
+    password="root", 
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true",
+      "driverClass=com.mysql.cj.jdbc.Driver"
+    }
+)
+
 @Named(value = "gestionnaireCompte")
 @RequestScoped
 public class GestionnaireCompte {
-
+    
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
     /**
      * Creates a new instance of GestionnaireCompte
      */
     public GestionnaireCompte() {
     }
     
+    
+    public List<CompteBancaire> getAllCustomers() {
+       Query query = em.createNamedQuery("CompteBancaire.findAll");
+       return query.getResultList();
+    }
+    
+    @Transactional
+    public void persist(CompteBancaire compte) {
+       em.persist(compte);
+    }
 }
